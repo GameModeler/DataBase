c304e8e0495988806a5422eb92783eaf1be8a3bc
nouk-pl <anne.plougastel@gmail.com>
2017-03-27 14:12:22 +0200

    Revert "#536"

---
+++ Dynamic/DynamicManager.cs
@@ -0,0 +1,116 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Dynamic
+{
+    public class DynamicManager
+    {
+        /// <summary>
+        /// Create IDictionnary object from mysql database
+        /// </summary>
+        /// <param name="user">mysql user</param>
+        /// <param name="pwd">mysql password</param>
+        /// <param name="database">mysql database</param>
+        /// <param name="table">mysql table</param>
+        /// <returns>IDictionnary object</returns>
+        public static IDictionary<string, object> CreateObjectByDatabase(string user, string pwd, string database, string table)
+        {
+
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=127.0.0.1;Uid= " + user + ";Pwd= " + pwd + ";Database= " + database + ";";
+            conn = new MySqlConnection();
+            try
+            {
+                conn = new MySqlConnection(myConnectionString);
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            string query = "USE " + database + "; SELECT * FROM " + table;
+            dynamic dynamicObject = new ExpandoObject();
+            Dictionary<string, string> results2 = CreateMySqlCommandDict(query, conn);
+            var dic = dynamicObject as IDictionary<string, object>;
+            foreach (var t in results2)
+            {
+                dic[t.Key] = t.Value;
+            }        
+            return dic;
+        }
+
+        /// <summary>
+        /// Create mysql command array
+        /// </summary>
+        /// <param name="myExecuteQuery">mysql query</param>
+        /// <param name="myConnection">MysqlConnection connection</param>
+        /// <param name="openclose">open and close mysql connection</param>
+        /// <returns>array of mysql return</returns>
+        public static string[] CreateMySqlCommandArray(string query, MySqlConnection connection, Boolean openclose = false)
+        {
+            MySqlCommand myCommand = new MySqlCommand(query, connection);
+            if (openclose)
+                myCommand.Connection.Open();
+            List<string> results = new List<string>();
+            using (var reader = myCommand.ExecuteReader())
+            {
+                int nbColumns = reader.VisibleFieldCount;
+                while (reader.Read())
+                {
+                    for (int i = 0; i < nbColumns; i++)
+                    {
+                        results.Add(reader.GetString(i));
+                    }
+                }
+            }
+
+            string[] array = results.ToArray();
+
+            if (openclose)
+                connection.Close();
+
+            return array;
+        }
+
+        /// <summary>
+        /// Create mysql command Dictionnary
+        /// </summary>
+        /// <param name="myExecuteQuery">mysql query</param>
+        /// <param name="myConnection">MysqlConnection connection</param>
+        /// <param name="openclose">open and close mysql connection</param>
+        /// <returns>Dictionnary of mysql return</returns>
+        public static Dictionary<string, string> CreateMySqlCommandDict(string query, MySqlConnection connection, Boolean openclose = false)
+        {
+            MySqlCommand myCommand = new MySqlCommand(query, connection);
+            if (openclose)
+                myCommand.Connection.Open();
+            Dictionary<string, string> results = new Dictionary<string, string>();
+            using (var reader = myCommand.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    int nbColumns = reader.VisibleFieldCount;
+                    Console.WriteLine(nbColumns);
+                    for(int i = 0; i < nbColumns; i++)
+                    {                                         
+                        results.Add(reader.GetName(i), reader.GetString(i));
+                    }
+                }
+            }
+
+            if (openclose)
+                connection.Close();
+
+            return results;
+        }
+
+    }
+}
---
+++ Yaml/YamlManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Yaml.Serialization;
+
+namespace DataBase.Yaml
+{
+    public class YamlManager
+    {
+
+        /// <summary>
+        /// Write T object to json file
+        /// </summary>
+        /// <typeparam name="T">T object</typeparam>
+        /// <param name="path">path</param>
+        /// <param name="fileName">file name</param>
+        /// <param name="objectToWrite">object to write</param>
+        /// <param name="append">append</param>
+        public static void WriteToYamlFile<T>(string path, string fileName, T objectToWrite, bool append = false) where T : new()
+        {
+            string filePath = path + fileName;
+            TextWriter writer = null;
+            try
+            {
+                var serializer = new YamlSerializer();
+                string contentsToWriteToFile = serializer.Serialize(objectToWrite);
+                writer = new StreamWriter(filePath, append);
+                writer.Write(contentsToWriteToFile);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+
+        /// <summary>
+        /// Read from json file and convert in T object
+        /// </summary>
+        /// <typeparam name="T">T object</typeparam>
+        /// <param name="path">path</param>
+        /// <param name="fileName">fileName</param>
+        /// <returns>T object</returns>
+        public static T ReadFromYamlFile<T>(string path, string fileName) where T : new()
+        {
+            string filePath = path + fileName;
+            TextReader reader = null;
+            try
+            {
+                var serializer = new YamlSerializer();
+                Console.WriteLine(serializer.DeserializeFromFile(filePath));
+                object obj = serializer.DeserializeFromFile(filePath)[0];
+                return (T)Convert.ChangeType(obj, typeof(T));
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+        }
+    }
+}
