52ca46a94c3258c28f6023f027b659932bb99c2a
anne <anne.plougastel@gmail.com>
2017-03-27 13:11:22 +0200

    updates

--- Entities/Car.cs
+++ Entities/Car.cs
@@ -1,10 +1,4 @@
 ﻿using DataBase.Utils;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataBase.Entities
 {
